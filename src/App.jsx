e3977ef8ac8eafba7befa32366f15e6223f95076
ndkhoi192 <157493147+ndkhoi192@users.noreply.github.com>
2025-07-10 01:01:53 +0700

    chore: update app and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ManagerDashboard from "./pages/manager/ManagerDashboard";
+import WarehousesDB from "./pages/manager/WarehousesDB";
 
+function App() {
   return (
-    <>
-      <h1>Khôi đần</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/manager" element={<ManagerDashboard />} />
+        <Route path="/manager/warehouse" element={<WarehousesDB />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
